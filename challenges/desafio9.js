99a21398a84b28b5e2f7267aab8b0a690ea4d3d5
ElaineCosta <elainerscosta@hotmail.com>
2022-10-17 16:43:09 -0300

    :fix: ajustando identacoes e virgulas para o Lint

--- challenges/desafio9.js
+++ challenges/desafio9.js
@@ -2,10 +2,10 @@ db.produtos.find(
   {
     valoresNutricionais: {
       $elemMatch: {
-        tipo: 'calorias',
-        quantidade: {$lt: 500 }
-      }
-    }
+        tipo: "calorias",
+        quantidade: { $lt: 500 },
+      },
+    },
   },
-  { _id: 0, nome: 1 }
+  { _id: 0, nome: 1 },
 );
